f8ffb65a8db7841378049b6f8b31420cdb3bde03
ctejaxun <ctejaxun@infonet.com.gt>
2020-08-19 21:13:09 -0600

    setea datos del feed atributo de un api get

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -27,13 +27,18 @@ import {
 
 const PushNotification = require('react-native-push-notification');
 
-const Panel = function () {
+const Panel = function ({feed: {created_at, field1, field2, field3, field4}}) {
   const [pesoTotal, setPesoTotal] = useState(0);
   const [liquidoNivel, setLiquidoNivel] = useState(1);
   const [alertaList, setAlertaList] = useState(2);
 
+  useEffect(() => {
+    setLiquidoNivel(LIQUIDO_CAPACIDAD - (field2 || 0));
+    setPesoTotal(field1 || 0);
+  });
+
   const isBuzonVacio = (peso) => peso === 0;
-  const LIQUIDO_CAPACIDAD = 100;
+  const LIQUIDO_CAPACIDAD = 20;
 
   const virtualH = (base, actualPer) => {
     const per = 100 - Math.min(100, (100 * actualPer) / base);
@@ -57,25 +62,43 @@ const Panel = function () {
   };
 
   return (() => {
-    if (!isBuzonVacio(pesoTotal)) {
-      PushNotification.localNotification({
-        message: mensajes.buzon.OCUPADO,
-      });
-    }
+    // if (!isBuzonVacio(pesoTotal)) {
+    //   PushNotification.localNotification({
+    //     message: mensajes.buzon.OCUPADO,
+    //   });
+    // }
 
-    const per100 = actualPer();
-    if (per100 === 0) {
-      PushNotification.localNotification({
-        message: mensajes.desinfectante.VACIO,
-      });
-    } else if (per100 <= 10) {
-      PushNotification.localNotification({
-        message: mensajes.desinfectante.BAJO,
-      });
+    // const per100 = actualPer();
+    // if (per100 === 0) {
+    //   PushNotification.localNotification({
+    //     message: mensajes.desinfectante.VACIO,
+    //   });
+    // } else if (per100 <= 10) {
+    //   PushNotification.localNotification({
+    //     message: mensajes.desinfectante.BAJO,
+    //   });
+    // }
+
+    const nivelMensaje = field4 || '';
+
+    if (nivelMensaje) {
+      if (
+        nivelMensaje === 'DepositoBajo' ||
+        nivelMensaje === 'DepositoMuyBajo'
+      ) {
+        PushNotification.localNotification({
+          message: mensajes.desinfectante.BAJO,
+        });
+      }
     }
 
     return (
       <View>
+        <Text>
+          {created_at
+            ? new Date(Date.parse(created_at)).toLocaleString('es-gt')
+            : 'no data'}
+        </Text>
         <View style={{height: 200, borderWidth: 4, position: 'relative'}}>
           <View style={{height: '50%', backgroundColor: '#9acd32'}}>
             <View
@@ -108,34 +131,64 @@ const Panel = function () {
           </Text>
         </View>
         <Text style={styles.title}>Buzon</Text>
-        <Text style={styles.dato}>
+        {/* <Text style={styles.dato}>
           {isBuzonVacio(pesoTotal) ? 'Vacio' : 'Ocupado'}
-        </Text>
-        <Text style={styles.dato}>Peso: {pesoTotal}</Text>
+        </Text> */}
+        <Text style={styles.dato}>{field3 || 'vacio'}</Text>
+        <Text style={styles.dato}>Peso: {pesoTotal} gramos</Text>
 
         <Text style={styles.title}>Desinfectante</Text>
         <Text style={styles.dato}>Liquido: {liquidoNivel}</Text>
         <Text style={styles.dato}>Capacidad: {LIQUIDO_CAPACIDAD}</Text>
-        <Button
+        {/* <Button
           onPress={() => {
             setLiquidoNivel(Math.floor(Math.random() * 100));
             setPesoTotal(Math.floor(Math.random() * 2));
           }}
           title="random valores"
-        />
+        /> */}
       </View>
     );
   })();
 };
 
+const obtenerFeed = async () => {
+  try {
+    const res = await fetch(
+      'https://api.thingspeak.com/channels/1117472/feeds.json?results=1',
+    );
+
+    let {
+      feeds: [first],
+    } = await res.json();
+
+    if (!first) {
+      first = {
+        created_at: new Date(),
+      };
+    }
+
+    return first;
+  } catch (error) {
+    alert('Ocurrio un error al obtener la información');
+  }
+};
+
 const App: () => React$Node = () => {
+  const [feed, setFeed] = useState(0);
+
+  window.setTimeout(async () => {
+    const f = await obtenerFeed();
+    setFeed(f);
+  }, 20000);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView contentInsetAdjustmentBehavior="automatic">
           <View style={{padding: 20}}>
-            <Panel />
+            <Panel feed={feed} />
           </View>
         </ScrollView>
       </SafeAreaView>
